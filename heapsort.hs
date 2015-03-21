653f6667d2615d0a7c423a65c57dadb850c73256
Thaís Freire Cavalcante <tfc2@cin.ufpe.br>
2015-03-21 19:35:32 -0300

    Swap reutilizavel

--- heapsort.hs
+++ heapsort.hs
@@ -14,10 +14,10 @@ createHeap :: Int -> [Int] -> [Int] -- cria o heap dada uma entrada
 createHeap position list
     | position == (length' list) + 1 = list
     | getElement position list <=  getPai position list = createHeap (position + 1) list
-    | otherwise = swap position list
+    | otherwise = createHeap (position `div` 2) (swap position list) -- retona a criacao a partir do pai para verificar se precisa de swap novamente
 
-swap :: Int -> [Int] -> [Int] -- faz a troca caso o no filho tenha numero maior que o pai e retona a criacao a partir do pai para verificar se precisa de swap novamente
-swap position list =  createHeap (position `div` 2) (inicio (position `div` 2) list ++ [getElement position list] ++ meio (position `div` 2) position list ++ [getPai position list] ++ fim position list)
+swap :: Int -> [Int] -> [Int] -- faz a troca caso o no filho tenha numero maior que o pai
+swap position list =  inicio (position `div` 2) list ++ [getElement position list] ++ meio (position `div` 2) position list ++ [getPai position list] ++ fim position list
 
 inicio :: Int -> [Int] -> [Int] -- auxilia o swap a pegar a lista antes o pai
 inicio 1 list = []
