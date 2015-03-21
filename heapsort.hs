e1c7b95709e06c944b7fba63f158b4585482d6cb
Thaís Freire Cavalcante <tfc2@cin.ufpe.br>
2015-03-21 19:10:21 -0300

    Mais correção de bug

--- heapsort.hs
+++ heapsort.hs
@@ -13,25 +13,23 @@ length' (a:as) = (length' as) + 1
 createTree :: Int -> [Int] -> [Int] -- cria uma arvore inicial dada uma entrada
 createTree position list
     | position == (length' list) + 1 = list -- caso base de chegar no fim da lista ou ela ser vazia
-    | getElement position list <=  getPai position list = create (position + 1) list
-    | otherwise = swap position list -- volta a criar a arvore verificando se o swap realmente terminou
+    | getElement position list <=  getPai position list = createTree (position + 1) list
+    | otherwise = swap position list
 
-swap :: Int -> [Int] -> [Int] -- faz a troca caso o no filho tenha numero maior que o pai
-swap position list
-    | getElement position list >  getPai position list = inicio position list : getElement position list : meio (position `div` 2) position list : getPai position list : fim position list
-    | otherwise = createTree (position `div` 2) list -- o swap volta a criar a arvore a partir da posicao do pai, para poder verificar se tem que fazer swap novamente
+swap :: Int -> [Int] -> [Int] -- faz a troca caso o no filho tenha numero maior que o pai e retonar a criacao a partir do pai para verificar se precisa de swap novamente
+swap position list =  createTree (position `div` 2) (inicio position list : getElement position list : meio (position `div` 2) position list : getPai position list : fim position list)
 
 inicio :: Int -> [Int] -> [Int] -- auxilia o swap a pegar a lista antes o pai
 inicio 1 list = []
-inicio position (a:as) = a : inicio (position - 1)
+inicio position (a:as) = a : inicio (position - 1) as
 
 meio :: Int -> Int -> [Int] -> [Int] -- auxilia o swap a pegar a lista entre pai e filho
 meio pai filho (a:as)
     | pai >= 1 = meio (pai - 1) (filho-1) as
-    | filho > 1 = a:(meio pai (filho-1)
+    | filho > 1 = a : meio pai (filho-1) as
     | otherwise = []
 
 fim :: Int -> [Int] -> [Int] -- auxilia o swap a pegar a lista depois do filho
 fim position (a:as)
     | position >= 1 = fim (position - 1) as
-    | otherwise = list
+    | otherwise = (a:as)
