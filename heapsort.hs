16f75ba0aa6e44247b48a1d26263cf1fb8d7662c
Thaís Freire Cavalcante <tfc2@cin.ufpe.br>
2015-03-21 18:06:40 -0300

    Correção de bugs

--- heapsort.hs
+++ heapsort.hs
@@ -1,20 +1,19 @@
-getElement :: Int -> [Int] -> Int -- considera a raiz como posicao 1
-getElement 1 (a:as) = a
+getElement :: Int -> [Int] -> Int -- retorna o numero da lista dada uma posicao
+getElement 1 (a:as) = a -- caso base, considera o primeira posicao como de numero 1
 getElement position (a:as) = getElement (position - 1) (as)
 
 
-getPai :: Int -> [Int] -> Int
-getPai 1 (a:as) = a
-getPai position (a:as) = getElement (position `div` 2) (a:as)
+getPai :: Int -> [Int] -> Int -- retorna o no pai de um numero dada sua posicao
+getPai 1 (a:as) = a -- caso base, o primeiro numero sendo pai dele mesmo
+getPai position (a:as) = getElement (position `div` 2) (a:as) -- pai = numero de posicao n/2 
 
-length' :: [Int] -> Int
+length' :: [Int] -> Int -- retorna o tamanho da lista
 length' [] = 0
 length' (a:as) = (length' as) + 1
 
-createTree :: Int -> [Int] -> [Int]
-createTree position [] = []
+createTree :: Int -> [Int] -> [Int] -- cria uma arvore inicial dada uma entrada
 createTree position list
-    | position = length + 1 = list
+    | position == (length list) + 1 = list -- caso base de chegar no fim da lista ou ela ser vazia
     | getElement position list <=  getPai position list = create (position + 1) list
     | otherwise = 0 -- criar funcao SWAP
 
