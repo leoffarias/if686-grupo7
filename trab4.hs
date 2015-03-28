fc81afbf274e06d47f772e4fdcce10cbc6dd7362
Thaís Freire Cavalcante <tfc2@cin.ufpe.br>
2015-03-28 15:43:10 -0300

    Questao 3
    Incompleto

--- trab4.hs
+++ trab4.hs
@@ -31,3 +31,58 @@ repeat' s n = repeat' (quebraLook s 1) (n-1)
 
 lookAndSay :: Int -> String
 lookAndSay n = repeat' "1" n
+
+----------- questao 3 -------------
+
+{-
+Defina um tipo de dados que representa um grafo não necessariamente conexo, onde cada nó tem um rótulo:
+type Rotulo = qualquer tipo de dado, ou seja, t -- nó
+type Aresta = (Rotulo, Rotulo), ou seja, (t,t) -- um aresta é a ligação entre dois nós, caso o nó não possua aresta o segundo rotulo é ele mesmo
+type Grafo = [Aresta], ou seja, [(t,t)] -- um grafo pode ser definido por um conjunto de arestas
+-}
+
+{-
+Exemplo de grafo: [(1,2), (2,3), (1,3), (2,4), (3,4), (5,5)], querendo percorrer entre nó 1 e 4
+-}
+
+{-
+corrigeGrafo:
+É necessário completar o grafo para que se vejam todos os caminhos possíveis, se tem (1,2), por exemplo, tem que ter (2,1).
+Além diso, é interessante remover os nós do tipo (x,x), para evitar loops inifinitos
+-}
+corrigeGrafo :: (Eq t) => [(t,t)] -> [(t,t)]
+corrigeGrafo [] = [] -- caso base
+corrigeGrafo ((x,y):as)
+    | x == y = corrigeGrafo as -- remove os elementos do tipo (x,x)
+    | contemAresta ((x,y):as) (y,x) == False = [(x,y),(y,x)] ++ (corrigeGrafo as)
+    | otherwise = (x,y):corrigeGrafo as
+
+contemAresta :: (Eq t) => [(t,t)] -> (t,t) -> Bool -- confere se há uma aresta no grafo
+contemAresta [] (x,y) = False -- caso base
+contemAresta ((x',y'):as) (x,y)
+    | (x,y) == (x',y') = True
+    | otherwise = contemAresta as (x,y)
+
+marcaVisitado :: (Eq t) => [(t,Bool)] -> t -> [(t,Bool)] 
+marcaVisitado [] r = []
+marcaVisitado ((r, v):as) rotulo
+    | r == rotulo = (r,True):marcaVisitado as rotulo
+    | otherwise = (r,v):marcaVisitado as rotulo
+
+buscaAdjacencia :: (Eq t) => [(t, t)] -> t -> [(t)] -- retorna vertices adjacentes a outro vertice (podem haver multiplos caminhos)
+buscaAdjacencia grafo r = [ x | (rotulo, x) <- grafo, rotulo == r]
+
+-- falta corrigir/terminar --
+
+busca :: (Eq t) => [(t,t)] -> [(t,Bool)] -> [t] -> t -> t -> [(t,t)] -- funcao de busca em profundidade
+busca grafo vertices pilha inicio fim = [] -- terminar!
+
+search :: (Eq t) => [(t,t)] -> t -> t -> [(t,t)] -- funcao inicial que corrige o grafo, lista os vertices, monta a pilha inicial e manda buscar
+search grafo inicio fim = busca (corrigeGrafo grafo) (listaVertices (corrigeGrafo grafo)) [inicio] inicio fim
+
+listaVertices :: (Eq t) => [(t,t)] -> [(t,Bool)] -- retorna a lista de vertices com a flag booleana de visitado = False
+listaVertices [] = [] -- caso base
+listaVertices ((x,y):as) = [(x,False)] ++ (listaVertices as) -- remover vertices repetidos
+
+-- https://www.youtube.com/watch?v=pJ3ilnhXWCQ
+-- http://www.land.ufrj.br/~classes/grafos/slides/aula_6.pdf
