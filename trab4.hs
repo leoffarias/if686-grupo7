a9721e39a4b0d05f58cc5f4283b7c89e591513e5
Leandro Farias <leoffarias@hotmail.com>
2015-03-27 21:52:23 -0300

    faltam tipos abstratos

--- trab4.hs
+++ trab4.hs
@@ -1,20 +1,15 @@
 ----------- questao 2 -------------
 
-{-showLook :: String -> Int
-showLook a:as
- | a == (head as) = 1 + showLook as
-
-look :: Int -> String -- n, int ou t?
-look 1 = "1"
-look n = [head (show n)]-}
-{-
-look :: Int -> String
-look 1 = quebraLook "1" 1
-look n = quebraLook n 1-}
-
 quebraLook :: String -> Int -> String
-quebraLook [] _ = []
 quebraLook (a:[]) n = (show n) ++ [a]
 quebraLook (a:as) n
  | a == (head as) = quebraLook as (1+n)
  | otherwise = (show n) ++ [a] ++ quebraLook as 1
+
+repeat' :: String -> Int -> String
+repeat' s 1 = "1"
+repeat' s 2 = quebraLook s 1
+repeat' s n = repeat' (quebraLook s 1) (n-1)
+
+lookAndSay :: Int -> String
+lookAndSay n = repeat' "1" n
