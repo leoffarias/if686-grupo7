9c20799eb96138a088fc8ef9e95a7fe66779f901
Leandro Farias <leoffarias@hotmail.com>
2015-03-30 12:10:33 -0300

    tipos genericos

--- trab4.hs
+++ trab4.hs
@@ -18,16 +18,16 @@ OLIVEIRA, Guilherme Gomes Neves de; PETRI, Renzo Augusto Lapelligrini. Haskell: 
 
 ----------- questao 2 -------------
 
-quebraLook :: String -> Int -> String
+quebraLook :: (Show t, Num t) => String -> t -> String
 quebraLook (a:[]) n = (show n) ++ [a]
 quebraLook (a:as) n
  | a == (head as) = quebraLook as (1+n)
  | otherwise = (show n) ++ [a] ++ quebraLook as 1
 
-repeat' :: String -> Int -> String
+repeat' :: (Eq t, Num t) => String -> t -> String
 repeat' s 1 = "1"
 repeat' s 2 = quebraLook s 1
 repeat' s n = repeat' (quebraLook s 1) (n-1)
 
-lookAndSay :: Int -> String
+lookAndSay :: (Eq t, Num t) => t -> String
 lookAndSay n = repeat' "1" n
