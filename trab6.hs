a3854cc873467284fc5732b6f5e25e6051a38584
Thaís Freire Cavalcante <tfc2@cin.ufpe.br>
2015-04-12 10:28:46 -0300

    Trab6
    Tentativas Q2

---
+++ trab6.hs
@@ -0,0 +1,13 @@
+data Grafos t = Nil | Grafo [(t, [(t,Int)])] deriving (Show, Eq) -- grafico representado por vertice e lista de (adjacencente, peso)
+
+-- para o map e o fold, consideramos que as funções são aplicadas nos vertices
+
+-- acho que ta errado, nao sei como testar essa funcao ai
+mapGraph :: (Grafos t -> Grafos t) -> Grafos t -> Grafos t
+mapGraph f Nil = Nil
+mapGraph f (Grafo ((vertice, adjacencias):as)) =  Grafo ( (f vertice, adjacencias) : mapGraph f (Grafo (as)) )
+
+-- acho que ta errado, nao sei como testar essa funcao ai
+foldGraph :: (t -> [Int] -> [Int]) -> Grafos t -> [Int]
+foldGraph f Nil = []
+foldGraph f (Grafo ((vertice, adjacencias):as)) = f (vertice) (foldGraph f (Grafo (as)))
