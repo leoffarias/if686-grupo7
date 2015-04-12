d5e5760e88cd6c15bb36e772b63c0d49b0f90b05
Thaís Freire Cavalcante <tfc2@cin.ufpe.br>
2015-04-12 10:57:16 -0300

    Trav 06 att

--- trab6.hs
+++ trab6.hs
@@ -2,12 +2,21 @@ data Grafos t = Nil | Grafo [(t, [(t,Int)])] deriving (Show, Eq) -- grafico repr
 
 -- para o map e o fold, consideramos que as funções são aplicadas nos vertices
 
+{-
 -- acho que ta errado, nao sei como testar essa funcao ai
 mapGraph :: (Grafos t -> Grafos t) -> Grafos t -> Grafos t
 mapGraph f Nil = Nil
 mapGraph f (Grafo ((vertice, adjacencias):as)) =  Grafo ( (f vertice, adjacencias) : mapGraph f (Grafo (as)) )
+-}
 
 -- acho que ta errado, nao sei como testar essa funcao ai
+foldGraph :: (t -> [t] -> [t]) -> t -> Grafos t -> [t]
+foldGraph f t Nil = [t]
+foldGraph f s (Grafo ((vertice, adjacencias):as)) = f (vertice) (foldGraph f s (Grafo (as)))
+
+
+{-
 foldGraph :: (t -> [Int] -> [Int]) -> Grafos t -> [Int]
 foldGraph f Nil = []
-foldGraph f (Grafo ((vertice, adjacencias):as)) = f (vertice) (foldGraph f (Grafo (as)))
+foldGraph f (Grafo ((vertice, adjacencias):as)) = f (vertice) (foldGraph f (Grafo (as))
+-}
